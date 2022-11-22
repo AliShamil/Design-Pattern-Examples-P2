c9252c55bc9425a4b2dcee7647155709832483a8
AliShamil <elisamilzade@gmail.com>
2022-11-23 02:22:47 +0400

    Final

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,110 @@
+﻿
+public interface IAircraft
+{
+    bool Airborne { get; }
+    void TakeOff();
+    int Height { get; }
+}
+
+
+// Target
+public sealed class Aircraft : IAircraft
+{
+    int height;
+    bool airborne;
+    public Aircraft()
+    {
+        height = 0;
+        airborne = false;
+    }
+    public void TakeOff()
+    {
+        Console.WriteLine("Aircraft engine takeoff");
+        airborne = true;
+        height = 200; // Meters
+    }
+    public bool Airborne
+    {
+        get { return airborne; }
+    }
+    public int Height
+    {
+        get { return height; }
+    }
+}
+// Adaptee interface
+public interface ISeacraft
+{
+    int Speed { get; }
+    void IncreaseRevs();
+}
+// Adaptee implementation
+public class Seacraft : ISeacraft
+{
+    int speed = 0;
+    public virtual void IncreaseRevs()
+    {
+        speed += 10;
+        Console.WriteLine("Seacraft engine increases revs to " + speed + " knots");
+    }
+    public int Speed
+    {
+        get { return speed; }
+    }
+}
+// Adapter
+public class Seabird : Seacraft, IAircraft
+{
+    int height = 0;
+
+    public void TakeOff()
+    {
+        while (!Airborne)
+            IncreaseRevs();
+    }
+    // Routes this straight back to the Aircraft
+    public int Height
+    {
+        get { return height; }
+    }
+    // This method is common to both Target and Adaptee
+    public override void IncreaseRevs()
+    {
+        base.IncreaseRevs();
+        if (Speed > 40)
+            height += 100;
+    }
+    public bool Airborne
+    {
+        get { return height > 50; }
+    }
+}
+class Experiment_MakeSeaBirdFly
+{
+    static void Main()
+    {
+        // No adapter
+        Console.WriteLine("Experiment 1: test the aircraft engine");
+        IAircraft aircraft = new Aircraft();
+        aircraft.TakeOff();
+        if (aircraft.Airborne) Console.WriteLine(
+        "The aircraft engine is fine, flying at "
+        +aircraft.Height+"meters");
+
+        // Classic usage of an adapter
+        Console.WriteLine("\nExperiment 2: Use the engine in the Seabird");
+        IAircraft seabird = new Seabird();
+
+        seabird.TakeOff(); // And automatically increases speed
+        Console.WriteLine("The Seabird took off");
+        // Two-way adapter: using seacraft instructions on an IAircraft object
+        // (where they are not in the IAircraft interface)
+        Console.WriteLine("\nExperiment 3: Increase the speed of the Seabird:");
+        (seabird as ISeacraft).IncreaseRevs();
+        (seabird as ISeacraft).IncreaseRevs();
+        if (seabird.Airborne)
+            Console.WriteLine("Seabird flying at height "+ seabird.Height +
+            " meters and speed "+(seabird as ISeacraft).Speed + " knots");
+        Console.WriteLine("Experiments successful; the Seabird flies!");
+    }
+}
--- Design Pattern Examples P2/Program.cs
+++ Design Pattern Examples P2/Program.cs
@@ -1,1 +1,160 @@
-﻿
+﻿namespace BuilderPattern;
+
+public class Engine
+{
+    public override string ToString() => "Simple Engine";
+}
+
+public class SportEngine : Engine
+{
+    public override string ToString() => "Sport Engine";
+}
+
+public interface ICarBuilder
+{
+    void Reset();
+    void SetSeats(int number);
+    void SetEngine(Engine engine);
+    void SetTripComputer();
+    void SetGPS();
+
+}
+
+public class CarBuilder : ICarBuilder
+{
+    private Car car = new();
+
+    public void Reset()
+    {
+        car = new();
+    }
+
+    public void SetEngine(Engine engine)
+    {
+        car.Engine = engine;
+    }
+
+    public void SetGPS()
+    {
+        car.GPS = "GPS for Simple Car";
+    }
+
+    public void SetSeats(int number)
+    {
+        car.Seats = number;
+    }
+
+    public void SetTripComputer()
+    {
+        car.TripComputer = "TripComputer for Simple Cars";
+    }
+
+    public Car GetResult() => car;
+}
+
+public class CarManualBuilder : ICarBuilder
+{
+    private CarManual manualCar = new();
+
+    public void Reset()
+    {
+        manualCar = new();
+    }
+
+    public void SetEngine(Engine engine)
+    {
+        manualCar.Engine = engine;
+    }
+
+    public void SetGPS()
+    {
+        manualCar.GPS = "GPS For Manual Car";
+    }
+
+    public void SetSeats(int number)
+    {
+        manualCar.Seats = number;
+    }
+
+    public void SetTripComputer()
+    {
+        manualCar.TripComputer = "TripComputer for Manual Cars";
+    }
+
+    public CarManual GetResult() => manualCar;
+}
+
+
+
+
+
+
+
+
+
+public class Car
+{
+    public int Seats { get; set; }
+    public string? TripComputer { get; set; }
+    public string? GPS { get; set; }
+    public Engine? Engine { get; set; }
+
+    public override string ToString()
+    =>$@"Seats count: {Seats}
+Trip Computer: {TripComputer??"EMPTY"}
+GPS: {GPS??"EMPTY"}
+Engine:{Engine}";
+    
+}
+
+public class CarManual : Car
+{
+
+}
+
+
+
+
+
+
+public class Director
+{
+    public CarManual MakeSUV(CarManualBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(4);
+        builder.SetEngine(new Engine());
+        return builder.GetResult();
+    }
+
+    public Car MakeSportCar(CarBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(2);
+        builder.SetEngine(new SportEngine());
+        builder.SetTripComputer();
+        builder.SetGPS();
+        return builder.GetResult();
+    }
+
+}
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Director director = new();
+        CarBuilder carBuilder = new();
+        CarManualBuilder carManualBuilder = new();
+
+        CarManual carManual = director.MakeSUV(carManualBuilder);
+        Car sportCar = director.MakeSportCar(carBuilder);
+        Console.WriteLine("---- CAR INFO ----");
+        Console.WriteLine(carManual);
+        Console.WriteLine();
+
+        Console.WriteLine(sportCar);
+    }
+}
